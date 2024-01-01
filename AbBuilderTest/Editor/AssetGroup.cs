4fb4bbcbc0b3985caf93784a89212ccb5445054c
Sibyl-null <1286438810@qq.com>
2024-01-01 23:49:26 +0800

    AbBuilderTest push

---
+++ AbBuilderTest/Editor/AssetGroup.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEditor.Build.Pipeline;
+using UnityEditor.Build.Pipeline.Interfaces;
+using UnityEngine;
+using UnityEngine.Build.Pipeline;
+using BuildCompression = UnityEngine.BuildCompression;
+
+namespace AbBuilderTest.Editor
+{
+    [CreateAssetMenu]
+    public class AssetGroup : ScriptableObject
+    {
+        [FolderPath] public string output;
+
+        public List<Sprite> sprites = new List<Sprite>();
+        public List<AudioClip> audioClips = new List<AudioClip>();
+        public List<Material> materials = new List<Material>();
+        public List<GameObject> prefabs = new List<GameObject>();
+        public List<Object> anyThings = new List<Object>();
+        
+        // --------------------------------------------
+        
+        [Button, PropertySpace]
+        private void BuildIn_Build()
+        {
+            string path = Path.Combine(output, "BuildIn");
+            if (Directory.Exists(path))
+                Directory.Delete(path, true);
+            Directory.CreateDirectory(path);
+
+            AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(path, 
+                GetAssetBundleBuilds(),
+                BuildAssetBundleOptions.None,
+                BuildTarget.StandaloneWindows);
+
+            AssetDatabase.Refresh();
+            EditorUtility.RevealInFinder(path);
+        }
+        
+        [Button]
+        private void CompatibilityBuildPipeline_Build()
+        {
+            string path = Path.Combine(output, "CompatibilityBuildPipeline");
+            if (Directory.Exists(path))
+                Directory.Delete(path, true);
+            Directory.CreateDirectory(path);
+
+            CompatibilityAssetBundleManifest manifest = CompatibilityBuildPipeline.BuildAssetBundles(path, 
+                GetAssetBundleBuilds(),
+                BuildAssetBundleOptions.None,
+                BuildTarget.StandaloneWindows);
+
+            AssetDatabase.Refresh();
+            EditorUtility.RevealInFinder(path);
+        }
+
+        [Button]
+        private void ContentPipeline_Build()
+        {
+            string path = Path.Combine(output, "ContentPipeline");
+            if (Directory.Exists(path))
+                Directory.Delete(path, true);
+            Directory.CreateDirectory(path);
+
+            AssetBundleBuild[] builds = GetAssetBundleBuilds();
+            BundleBuildContent buildContent = new BundleBuildContent(builds);
+
+            BundleBuildParameters buildParameters =
+                new BundleBuildParameters(BuildTarget.Android, BuildTargetGroup.Android, path)
+                    {
+                        BundleCompression = BuildCompression.LZ4,
+                        UseCache = false,
+                        WriteLinkXML = true
+                    };
+
+            IList<IBuildTask> taskList = DefaultBuildTasks.Create(DefaultBuildTasks.Preset.AssetBundleCompatible);
+
+            ContentPipeline.BuildCallbacks.PostScriptsCallbacks += OnPostScriptsCallback;
+
+            ContentPipeline.BuildAssetBundles(buildParameters, buildContent, 
+                    out IBundleBuildResults results, taskList);
+
+            AssetDatabase.Refresh();
+            EditorUtility.RevealInFinder(path);
+        }
+
+        private ReturnCode OnPostScriptsCallback(IBuildParameters parameters, IBuildResults results)
+        {
+            foreach (string assembly in results.ScriptResults.assemblies)
+                Debug.Log(assembly);
+
+            return ReturnCode.Success;
+        }
+
+        private AssetBundleBuild[] GetAssetBundleBuilds()
+        {
+            List<AssetBundleBuild> builds = new List<AssetBundleBuild>
+            {
+                new AssetBundleBuild
+                {
+                    assetBundleName = "Sprites",
+                    assetNames = sprites.Select(AssetDatabase.GetAssetPath).ToArray()
+                },
+                new AssetBundleBuild()
+                {
+                    assetBundleName = "AudioClips",
+                    assetNames = audioClips.Select(AssetDatabase.GetAssetPath).ToArray(),
+                    addressableNames = audioClips.Select(x => Path.GetFileName(AssetDatabase.GetAssetPath(x))).ToArray()
+                },
+                new AssetBundleBuild()
+                {
+                    assetBundleName = "Materials",
+                    assetNames = materials.Select(AssetDatabase.GetAssetPath).ToArray()
+                },
+                new AssetBundleBuild()
+                {
+                    assetBundleName = "Prefabs",
+                    assetNames = prefabs.Select(AssetDatabase.GetAssetPath).ToArray()
+                },
+                new AssetBundleBuild()
+                {
+                    assetBundleName = "AnyThings",
+                    assetNames = anyThings.Select(AssetDatabase.GetAssetPath).ToArray()
+                }
+            };
+            return builds.ToArray();
+        }
+    }
+}
---
+++ AbBuilderTest/Editor/DepCompareWindow.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Serialization;
+using UnityEditor;
+using UnityEditor.Build.Content;
+using UnityEditor.Build.Pipeline;
+using UnityEditor.Build.Player;
+using UnityEngine;
+
+namespace AbBuilderTest.Editor
+{
+    public class DepCompareWindow : OdinEditorWindow
+    {
+        [MenuItem("Tools/Open DepCompareWindow")]
+        private static void ShowWindow()
+        {
+            GetWindow<DepCompareWindow>();
+        }
+
+        [OdinSerialize] private Object _asset;
+        
+        // -------------------------------------------------------------------------------------
+        
+        [OdinSerialize, TitleGroup("AssetDatabase")] 
+        private readonly Dictionary<string, Object> _assetDatabaseDepMap = new();
+
+        [Button, TitleGroup("AssetDatabase")]
+        public void FindDepByAssetDatabase()
+        {
+            string assetPath = AssetDatabase.GetAssetPath(_asset);
+            string[] depPaths = AssetDatabase.GetDependencies(assetPath);
+            
+            _assetDatabaseDepMap.Clear();
+            foreach (string depPath in depPaths)
+            {
+                if (depPath == assetPath)
+                    continue;
+                
+                _assetDatabaseDepMap.Add(depPath, AssetDatabase.LoadMainAssetAtPath(depPath));
+            }
+        }
+        
+        // -------------------------------------------------------------------------------------
+        
+        [OdinSerialize, TitleGroup("ContentBuildInterface")] 
+        private readonly Dictionary<string, Object> _contentBuildDepMap = new();
+        
+        [Button, TitleGroup("ContentBuildInterface")]
+        public void FindDepByContentBuild()
+        {
+            TypeDB typeDB = PlayerBuildInterface.CompilePlayerScripts(new ScriptCompilationSettings
+            {
+                group = BuildTargetGroup.Standalone,
+                target = BuildTarget.StandaloneWindows,
+                options = ScriptCompilationOptions.None
+            }, ContentPipeline.kScriptBuildPath).typeDB;
+
+            if (!ObjectIdentifier.TryGetObjectIdentifier(_asset, out ObjectIdentifier id)) 
+                return;
+
+            ObjectIdentifier[] objectIdentifiers =
+                ContentBuildInterface.GetPlayerDependenciesForObject(id, BuildTarget.StandaloneWindows, typeDB);
+
+            _contentBuildDepMap.Clear();
+            foreach (ObjectIdentifier identifier in objectIdentifiers)
+            {
+                if (identifier.guid == id.guid)
+                    continue;
+
+                Object assetObject = ObjectIdentifier.ToObject(identifier);
+                if (assetObject != null)
+                {
+                    string path = AssetDatabase.GetAssetPath(assetObject);
+                    _contentBuildDepMap.TryAdd(path, assetObject);
+                }
+                else
+                {
+                    Debug.Log("ToObject Failed: " + identifier);
+                }
+            }
+        }
+    }
+}
