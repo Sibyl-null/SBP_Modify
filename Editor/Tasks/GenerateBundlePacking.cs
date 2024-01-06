62680c53482bb5a98c34b76dfa0a0a559e2a622a
Sibyl-null <1286438810@qq.com>
2024-01-06 23:57:00 +0800

    GenerateBundlePacking.cs changed

--- Editor/Tasks/GenerateBundlePacking.cs
+++ Editor/Tasks/GenerateBundlePacking.cs
@@ -10,90 +10,91 @@ using UnityEditor.Build.Utilities;
 namespace UnityEditor.Build.Pipeline.Tasks
 {
     /// <summary>
-    /// Packs each asset bundle and calculates the asset load file dependency list.
+    /// 组装每个 AssetBundle 并计算资产加载文件依赖项列表.
     /// </summary>
     public class GenerateBundlePacking : IBuildTask
     {
-        /// <inheritdoc />
-        public int Version { get { return 1; } }
+        public int Version => 1;
 
-#pragma warning disable 649
-        [InjectContext(ContextUsage.In)]
-        IBundleBuildContent m_BuildContent;
-
-        [InjectContext(ContextUsage.In)]
-        IDependencyData m_DependencyData;
-
-        [InjectContext]
-        IBundleWriteData m_WriteData;
-
-        [InjectContext(ContextUsage.In)]
-        IDeterministicIdentifiers m_PackingMethod;
-
-#if UNITY_2019_3_OR_NEWER
-        [InjectContext(ContextUsage.In, true)]
-        ICustomAssets m_CustomAssets;
-#endif
-#pragma warning restore 649
+        [InjectContext(ContextUsage.In)] private IBundleBuildContent m_BuildContent;
+        [InjectContext(ContextUsage.In)] private IDependencyData m_DependencyData;
+        [InjectContext(ContextUsage.In)] private IDeterministicIdentifiers m_PackingMethod;
+        [InjectContext(ContextUsage.In, true)] private ICustomAssets m_CustomAssets;
+        [InjectContext] private IBundleWriteData m_WriteData;
 
         static bool ValidAssetBundle(List<GUID> assets, HashSet<GUID> customAssets)
         {
-            // Custom Valid Asset Bundle function that tests if every asset is known by the asset database, is an asset (not a scene), or is a user driven custom asset
-            return assets.All(x => ValidationMethods.ValidAsset(x) == ValidationMethods.Status.Asset || customAssets.Contains(x));
+            // 使用 AssetDatabase 确认是否所有的资产都是 asset 而不是 scene，或者是 custom asset
+            return assets.All(x =>
+                ValidationMethods.ValidAsset(x) == ValidationMethods.Status.Asset || customAssets.Contains(x));
         }
 
-        /// <inheritdoc />
         public ReturnCode Run()
         {
             Dictionary<GUID, List<GUID>> assetToReferences = new Dictionary<GUID, List<GUID>>();
             HashSet<GUID> customAssets = new HashSet<GUID>();
-#if UNITY_2019_3_OR_NEWER
             if (m_CustomAssets != null)
                 customAssets.UnionWith(m_CustomAssets.Assets);
-#endif
 
-            // Pack each asset bundle
-            foreach (var bundle in m_BuildContent.BundleLayout)
-            {
-                if (ValidAssetBundle(bundle.Value, customAssets))
-                    PackAssetBundle(bundle.Key, bundle.Value, assetToReferences);
-                else if (ValidationMethods.ValidSceneBundle(bundle.Value))
-                    PackSceneBundle(bundle.Key, bundle.Value, assetToReferences);
-            }
-
-            // Calculate Asset file load dependency list
-            foreach (var bundle in m_BuildContent.BundleLayout)
-            {
-                foreach (var asset in bundle.Value)
-                {
-                    List<string> files = m_WriteData.AssetToFiles[asset];
-                    List<GUID> references = assetToReferences[asset];
-                    foreach (var reference in references)
-                    {
-                        List<string> referenceFiles = m_WriteData.AssetToFiles[reference];
-                        if (!files.Contains(referenceFiles[0]))
-                            files.Add(referenceFiles[0]);
-                    }
-                }
-            }
+            PackAllBundles(customAssets, assetToReferences);
+            CalculateLoadDependencyList(assetToReferences);
 
             return ReturnCode.Success;
         }
 
+        private void PackAllBundles(HashSet<GUID> customAssets, Dictionary<GUID, List<GUID>> assetToReferences)
+        {
+            foreach (KeyValuePair<string, List<GUID>> bundle in m_BuildContent.BundleLayout)
+            {
+                if (ValidAssetBundle(bundle.Value, customAssets)) // 资源组装
+                    PackAssetBundle(bundle.Key, bundle.Value, assetToReferences);
+                else if (ValidationMethods.ValidSceneBundle(bundle.Value)) // 场景组装
+                    PackSceneBundle(bundle.Key, bundle.Value, assetToReferences);
+            }
+        }
+        
+        private void CalculateLoadDependencyList(Dictionary<GUID, List<GUID>> assetToReferences)
+        {
+            // Calculate Asset file load dependency list
+            foreach (List<GUID> guids in m_BuildContent.BundleLayout.Values)
+            {
+                foreach (GUID asset in guids)
+                {
+                    List<string> depFiles = m_WriteData.AssetToFiles[asset];
+                    
+                    foreach (GUID reference in assetToReferences[asset])
+                    {
+                        List<string> referenceFiles = m_WriteData.AssetToFiles[reference];
+                        
+                        // referenceFiles[0] 是该 asset 的主文件
+                        if (!depFiles.Contains(referenceFiles[0]))
+                            depFiles.Add(referenceFiles[0]);
+                    }
+                }
+            }
+        }
+
         void PackAssetBundle(string bundleName, List<GUID> includedAssets, Dictionary<GUID, List<GUID>> assetToReferences)
         {
-            var internalName = string.Format(CommonStrings.AssetBundleNameFormat, m_PackingMethod.GenerateInternalFileName(bundleName));
+            // CommonStrings.AssetBundleNameFormat = "archive:/{0}/{0}"
+            string internalName = string.Format(CommonStrings.AssetBundleNameFormat,
+                m_PackingMethod.GenerateInternalFileName(bundleName));
 
-            var allObjects = new HashSet<ObjectIdentifier>();
-            Dictionary<GUID, HashSet<ObjectIdentifier>> assetObjectIdentifierHashSets = new Dictionary<GUID, HashSet<ObjectIdentifier>>();
-            foreach (var asset in includedAssets)
+            HashSet<ObjectIdentifier> allObjects = new HashSet<ObjectIdentifier>();
+            Dictionary<GUID, HashSet<ObjectIdentifier>> assetObjectIdentifierHashSets =
+                new Dictionary<GUID, HashSet<ObjectIdentifier>>();
+            
+            // 遍历 bundle 内所有的 asset（反映的是外部设定的 AssetBundleBuild ）
+            foreach (GUID asset in includedAssets)
             {
                 AssetLoadInfo assetInfo = m_DependencyData.AssetInfo[asset];
                 allObjects.UnionWith(assetInfo.includedObjects);
 
-                var references = new List<ObjectIdentifier>();
+                List<ObjectIdentifier> references = new List<ObjectIdentifier>();
                 references.AddRange(assetInfo.referencedObjects);
-                assetToReferences[asset] = FilterReferencesForAsset(m_DependencyData, asset, references, null, null, assetObjectIdentifierHashSets);
+                
+                assetToReferences[asset] = FilterReferencesForAsset(asset, references, 
+                    null, null, assetObjectIdentifierHashSets);
 
                 allObjects.UnionWith(references);
                 m_WriteData.AssetToFiles[asset] = new List<string> { internalName };
@@ -125,7 +126,8 @@ namespace UnityEditor.Build.Pipeline.Tasks
 
                 var references = new List<ObjectIdentifier>();
                 references.AddRange(sceneInfo.referencedObjects);
-                assetToReferences[scene] = FilterReferencesForAsset(m_DependencyData, scene, references, previousSceneObjects, previousSceneAssets, assetObjectIdentifierHashSets);
+                assetToReferences[scene] = FilterReferencesForAsset(scene, references, previousSceneObjects,
+                    previousSceneAssets, assetObjectIdentifierHashSets);
                 previousSceneObjects.UnionWith(references);
                 previousSceneAssets.UnionWith(assetToReferences[scene]);
 
@@ -143,7 +145,7 @@ namespace UnityEditor.Build.Pipeline.Tasks
         static HashSet<ObjectIdentifier> GetRefObjectIdLookup(AssetLoadInfo referencedAsset, Dictionary<GUID, HashSet<ObjectIdentifier>> assetObjectIdentifierHashSets)
         {
             HashSet<ObjectIdentifier> refObjectIdLookup;
-            if ((assetObjectIdentifierHashSets == null) || (!assetObjectIdentifierHashSets.TryGetValue(referencedAsset.asset, out refObjectIdLookup)))
+            if (assetObjectIdentifierHashSets == null || !assetObjectIdentifierHashSets.TryGetValue(referencedAsset.asset, out refObjectIdLookup))
             {
                 refObjectIdLookup = new HashSet<ObjectIdentifier>(referencedAsset.referencedObjects);
                 assetObjectIdentifierHashSets?.Add(referencedAsset.asset, refObjectIdLookup);
@@ -151,17 +153,23 @@ namespace UnityEditor.Build.Pipeline.Tasks
             return refObjectIdLookup;
         }
 
-        internal static List<GUID> FilterReferencesForAsset(IDependencyData dependencyData, GUID asset, List<ObjectIdentifier> references, HashSet<ObjectIdentifier> previousSceneObjects = null, HashSet<GUID> previousSceneReferences = null, Dictionary<GUID, HashSet<ObjectIdentifier>> assetObjectIdentifierHashSets = null)
+        private List<GUID> FilterReferencesForAsset(GUID asset, List<ObjectIdentifier> references, 
+            HashSet<ObjectIdentifier> previousSceneObjects = null, HashSet<GUID> previousSceneReferences = null, 
+            Dictionary<GUID, HashSet<ObjectIdentifier>> assetObjectIdentifierHashSets = null)
         {
-            var referencedAssets = new HashSet<AssetLoadInfo>();
-            var referencedAssetsGuids = new List<GUID>(referencedAssets.Count);
-            var referencesPruned = new List<ObjectIdentifier>(references.Count);
-            // Remove Default Resources and Includes for Assets assigned to Bundles
+            HashSet<AssetLoadInfo> referencedAssets = new HashSet<AssetLoadInfo>();
+            List<GUID> referencedAssetsGuids = new List<GUID>(referencedAssets.Count);
+            List<ObjectIdentifier> referencesPruned = new List<ObjectIdentifier>(references.Count);
+            
+            // 剔除内置资源引用
             foreach (ObjectIdentifier reference in references)
             {
+                // CommonStrings.UnityDefaultResourcePath = "library/unity default resources"
+                // 忽略大小写的字符串比较
                 if (reference.filePath.Equals(CommonStrings.UnityDefaultResourcePath, StringComparison.OrdinalIgnoreCase))
                     continue;
-                if (dependencyData.AssetInfo.TryGetValue(reference.guid, out AssetLoadInfo referenceInfo))
+                
+                if (m_DependencyData.AssetInfo.TryGetValue(reference.guid, out AssetLoadInfo referenceInfo))
                 {
                     if (referencedAssets.Add(referenceInfo))
                         referencedAssetsGuids.Add(referenceInfo.asset);
@@ -169,6 +177,7 @@ namespace UnityEditor.Build.Pipeline.Tasks
                 }
                 referencesPruned.Add(reference);
             }
+            
             references.Clear();
             references.AddRange(referencesPruned);
 
@@ -176,25 +185,17 @@ namespace UnityEditor.Build.Pipeline.Tasks
             // Remove References also included by circular Referenced Assets if Asset's GUID is higher than Referenced Asset's GUID
             foreach (AssetLoadInfo referencedAsset in referencedAssets)
             {
-                if ((asset > referencedAsset.asset) || (asset == referencedAsset.asset))
+                if (asset > referencedAsset.asset || asset == referencedAsset.asset)
                 {
                     references.RemoveAll(GetRefObjectIdLookup(referencedAsset, assetObjectIdentifierHashSets).Contains);
                 }
                 else
                 {
-                    bool exists = false;
-                    foreach (ObjectIdentifier referencedObject in referencedAsset.referencedObjects)
-                    {
-                        if (referencedObject.guid == asset)
-                        {
-                            exists = true;
-                            break;
-                        }
-                    }
+                    bool exists =
+                        referencedAsset.referencedObjects.Any(referencedObject => referencedObject.guid == asset);
+                    
                     if (!exists)
-                    {
                         references.RemoveAll(GetRefObjectIdLookup(referencedAsset, assetObjectIdentifierHashSets).Contains);
-                    }
                 }
             }
 
@@ -203,12 +204,11 @@ namespace UnityEditor.Build.Pipeline.Tasks
             {
                 foreach (GUID reference in previousSceneReferences)
                 {
-                    if (!dependencyData.AssetInfo.TryGetValue(reference, out AssetLoadInfo referencedAsset))
+                    if (!m_DependencyData.AssetInfo.TryGetValue(reference, out AssetLoadInfo referencedAsset))
                         continue;
 
                     var refObjectIdLookup = GetRefObjectIdLookup(referencedAsset, assetObjectIdentifierHashSets);
-                    // NOTE: It's impossible for an asset to depend on a scene, thus no need for circular reference checks
-                    // So just remove and add a dependency on the asset if there is a need to depend upon it.
+                    // NOTE: 资产不可能依赖于场景，因此不需要循环参考检查，所以如果需要依赖它，只需删除并添加对资产的依赖
                     if (references.RemoveAll(refObjectIdLookup.Contains) > 0)
                         referencedAssetsGuids.Add(referencedAsset.asset);
                 }
@@ -217,6 +217,7 @@ namespace UnityEditor.Build.Pipeline.Tasks
             // Special path for scenes, they can use data from previous sharedAssets in the same bundle
             if (!previousSceneObjects.IsNullOrEmpty())
                 references.RemoveAll(previousSceneObjects.Contains);
+            
             return referencedAssetsGuids;
         }
     }
