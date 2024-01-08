a0bd2482b4aaea061ae72d8eeb74f2b48aae65b4
yangboyuan <yangboyuan@dailyinnovation.biz>
2024-01-08 10:06:04 +0800

    StripUnusedSpriteSources.cs changed

--- Editor/Tasks/StripUnusedSpriteSources.cs
+++ Editor/Tasks/StripUnusedSpriteSources.cs
@@ -7,25 +7,16 @@ using UnityEditor.Build.Pipeline.Interfaces;
 namespace UnityEditor.Build.Pipeline.Tasks
 {
     /// <summary>
-    /// Removes all unusued sprite source data from asset references and scene references.
+    /// 从资源引用和场景引用中删除所有未使用的 sprite source data
     /// </summary>
     public class StripUnusedSpriteSources : IBuildTask
     {
-        /// <inheritdoc />
-        public int Version { get { return 2; } }
+        public int Version => 2;
 
-#pragma warning disable 649
-        [InjectContext]
-        IDependencyData m_DependencyData;
+        [InjectContext] private IDependencyData m_DependencyData;
+        [InjectContext(ContextUsage.In, true)] private IBuildSpriteData m_SpriteData;
+        [InjectContext(ContextUsage.InOut, true)] private IBuildExtendedAssetData m_ExtendedAssetData;
 
-        [InjectContext(ContextUsage.In, true)]
-        IBuildSpriteData m_SpriteData;
-
-        [InjectContext(ContextUsage.InOut, true)]
-        IBuildExtendedAssetData m_ExtendedAssetData;
-#pragma warning restore 649
-
-        /// <inheritdoc />
         public ReturnCode Run()
         {
             if (m_SpriteData == null || m_SpriteData.ImporterData.Count == 0)
@@ -34,8 +25,9 @@ namespace UnityEditor.Build.Pipeline.Tasks
             if (EditorSettings.spritePackerMode == SpritePackerMode.Disabled)
                 return ReturnCode.SuccessNotRun;
 
-            var unusedSources = new HashSet<ObjectIdentifier>();
-            var textures = m_SpriteData.ImporterData.Values.Where(x => x.PackedSprite).Select(x => x.SourceTexture);
+            HashSet<ObjectIdentifier> unusedSources = new HashSet<ObjectIdentifier>();
+            IEnumerable<ObjectIdentifier> textures = m_SpriteData.ImporterData.Values.Where(x => x.PackedSprite)
+                .Select(x => x.SourceTexture);
             unusedSources.UnionWith(textures);
 
             // Count refs from assets
